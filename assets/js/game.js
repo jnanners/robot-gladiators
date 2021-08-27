04878db26c48f757d9c89918ffc6353b6154a2ed
JBanana <johnandrewharris3@gmail.com>
2021-08-27 13:07:51 -0500

    added highscore mechanic

--- assets/js/game.js
+++ assets/js/game.js
@@ -31,47 +31,55 @@ var fight = function(enemy) {
     //keep trck of who goes first
     var isPlayerTurn = true;
 
+    //randomly change turn order
     if(Math.random() > 0.5) {
         isPlayerTurn = false;
     }
     
     //repeat and execute as long as the enemy-robot is alive
     while(playerInfo.health > 0 && enemy.health > 0) {
-        if(fightOrSkip()) {
-            break;
+        if(isPlayerTurn) {
+            if(fightOrSkip()) {
+                break;
+            }
+            //generate random damage value based on a player's attack power
+            var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+            enemy.health = Math.max(0, enemy.health - damage)
+            //log a resulting message in the console to confirm it worked
+            console.log(
+                playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining."
+            );
+            //check enemy's health
+            if (enemy.health <=0) {
+                window.alert(enemy.name + " has died!");
+                playerInfo.money = playerInfo.money +20;
+                break;
+            }
+            else {
+                window.alert(enemy.name + " still has " + enemy.health + " health left.");
+            }
+            //player gets attacked first
         }
-        //generate random damage value based on a player's attack power
-        var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
-        enemy.health = Math.max(0, enemy.health - damage)
-        //log a resulting message in the console to confirm it worked
-        console.log(
-            playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining."
-        );
-        //check enemy's health
-        if (enemy.health <=0) {
-            window.alert(enemy.name + " has died!");
-            playerInfo.money = playerInfo.money +20;
-            break;
-        }
-        else {
-            window.alert(enemy.name + " still has " + enemy.health + " health left.");
-        }
-        //subtract the value of enemy.attack from the value of playerInfo.health and use that result to update the value in the playerInfo.health variable
-        var damage = randomNumber(enemy.attack - 3, enemy.attack);
+        else{
+            //subtract the value of enemy.attack from the value of playerInfo.health and use that result to update the value in the playerInfo.health variable
+            var damage = randomNumber(enemy.attack - 3, enemy.attack);
 
-        playerInfo.health = Math.max(0, playerInfo.health - damage);
-        //log a resulting message in the console to confirm it worked
-        console.log(
-            enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining."
-        );
-        //check player's health
-        if (playerInfo.health <=0) {
-            window.alert(playerInfo.name + " has died!");
-            break;
+            playerInfo.health = Math.max(0, playerInfo.health - damage);
+            //log a resulting message in the console to confirm it worked
+            console.log(
+                enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining."
+            );
+            //check player's health
+            if (playerInfo.health <=0) {
+                window.alert(playerInfo.name + " has died!");
+                break;
+            }
+            else {
+                window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
+            }
         }
-        else {
-            window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
-        }
+        //switch turn order for next round
+        isPlayerTurn = !isPlayerTurn;
     }
 };
 
@@ -110,21 +118,31 @@ var startGame = function() {
 
 //function to end the entire game
 var endGame = function() {
-    // if player still alive, player wins
-    if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+    window.alert("The game has now ended. Let's see how you did!");
+
+    //check localStorage for high score, if it's not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    if(highScore === null) {
+        highScore = 0;
     }
-    else {
-        window.alert("You've lost your robot in battle.");
+    //if player has more moeny than the high score, player has new highscore
+    if(playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
     }
-    //ask player if they want to play again
+    else{
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+    }
+
+    //ask player if they'd like to play again
     var playAgainConfirm = window.confirm("Would you like to play again?");
 
     if(playAgainConfirm) {
-        //restart the game
         startGame();
     }
-    else {
+    else{
         window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
 };
